1d621b6195dc97b2e39869fb7ec43062354c9ecb
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-06-27 00:48:45 +0000

    Automatically backed up by Learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,19 @@
+require 'rack-flash'
+
+class SongsController < ApplicationController
+  use Rack::Flash
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'songs/index'
+  end
+
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
