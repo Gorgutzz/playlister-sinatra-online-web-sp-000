604a39d8f5ccf184ace6c9ea4b719a452fa808d1
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-06-27 01:04:24 +0000

    Automatically backed up by Learn

--- db/migrate/20190627010301_create_songs.rb
+++ db/migrate/20190627010301_create_songs.rb
@@ -1,8 +1,8 @@
-class CreateSongs < ActiveRecord::Migration[5.2]
+class CreateSongGenresJoinTable < ActiveRecord::Migration[5.2]
   def change
-    create_table :songs do |t|
-      t.string :name
-      t.string :artist_id
+    create_table :song_genres do |t|
+      t.string :song_id
+      t.string :genre_id
     end
   end
 end
