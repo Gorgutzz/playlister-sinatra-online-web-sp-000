9750330a17d582fcb1b7e2f72ef5f937d5f6338a
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-06-27 00:53:08 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,6 @@ class Song < ActiveRecord::Base
   has_many :genres, through: :song_genres
   belongs_to :artist
 
-  #The gsub replaces spaces with hyphens, downcase makes it lowercase.
   def slug
     self.name.gsub(" ", "-").downcase
   end
